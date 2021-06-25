44a99a25f24613317ab16150aa9b1080e1d6702c
ihposey12 <ihposey@crimson.ua.edu>
2021-06-25 16:42:37 -0700

    mapped todos, client side set up

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,9 @@
-const Todo = () => {
+const Todo = (props) => {
+    const { title } = props.todo
+
     return (
         <h3 className='text-dark-center text-center p-1 bg-light border-bottom'>
-            <i className='far fa-times-circle fa-sm float-left m-1 text-danger'></i>Task
+            <i className='far fa-times-circle fa-sm float-left m-1 text-danger'></i>{title}
             <input type='checkbox' className='m-2 float-right' />
         </h3>
     )
